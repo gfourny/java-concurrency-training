a5ee4904d68ffa6760918bd32bfa7402b320dde4
Guillaume Fourny <125885296+gfourny-sfeir@users.noreply.github.com>
2024-02-20 13:38:38 +0100

    feat: initialisation du projet (#1)

---
+++ src/main/java/fr/concurrency/training/TrainingApplication.java
@@ -0,0 +1,13 @@
+package fr.concurrency.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/concurrency/training/TrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.concurrency.training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
