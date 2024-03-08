deed5744daea798870fd5222168dfb5d6834ab7c
Guillaume Fourny <125885296+gfourny-sfeir@users.noreply.github.com>
2024-03-08 17:23:18 +0100

    feat: Danger des VirtualThread

--- src/main/java/fr/concurrency/training/TrainingApplication.java
+++ src/main/java/fr/concurrency/training/TrainingApplication.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class TrainingApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/fr/concurrency/training/model/Customer.java
@@ -0,0 +1,41 @@
+package fr.concurrency.training.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * @author gfourny
+ */
+@Entity
+@Table(name = "customers")
+@Setter
+@Getter
+@ToString
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    public Customer() {
+    }
+
+    public Customer(Long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+}
