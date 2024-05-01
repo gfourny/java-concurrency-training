5b1781caa460be04257e39d550546dee2ed1e4c7
Guillaume Fourny <fourny.g@free.fr>
2024-05-01 15:35:34 +0200

    feat: utilisation des VirtualThread avec Semaphore

--- src/main/java/fr/concurrency/training/service/CustomerService.java
+++ src/main/java/fr/concurrency/training/service/CustomerService.java
@@ -1,16 +1,17 @@
 package fr.concurrency.training.service;
 
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
+import fr.concurrency.training.model.Customer;
+import fr.concurrency.training.repository.CustomerRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.instancio.Instancio;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import fr.concurrency.training.model.Customer;
-import fr.concurrency.training.repository.CustomerRepository;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
 
 import static org.instancio.Select.field;
 
@@ -18,20 +19,23 @@ import static org.instancio.Select.field;
  * @author gfourny
  */
 @Service
+@Slf4j
 public class CustomerService {
 
     private final CustomerRepository customerRepository;
     private final ExecutorService executorService;
+    private final Semaphore semaphore; //On injecte un Semaphore
 
     public CustomerService(
             CustomerRepository customerRepository,
             @Value("${spring.datasource.hikari.maximum-pool-size}") int poolSize) {
         this.customerRepository = customerRepository;
-        this.executorService = Executors.newFixedThreadPool(poolSize); //On se base sur le nombre de pool de connexion maximum
+        this.semaphore = new Semaphore(poolSize); //On se base sur le nombre de pool de connexion maximum
+        this.executorService = Executors.newVirtualThreadPerTaskExecutor();
     }
 
     /**
-     * On doit définir un pool de thread qui a pour taille le nombre maximum de pool de connexion
+     * On doit définir un pool de permit qui a pour taille le nombre maximum de pool de connexion
      *
      * @return {@link List} de {@link Customer}
      */
@@ -45,14 +49,28 @@ public class CustomerService {
                 .generate(field(Customer::getEmail), gen -> gen.net().email())
                 .create();
 
-        /*
-         * La structure doit rester la même, par démonstration, on n'utilise pas la méthode {@link CustomerRepository#saveAll(Iterable)}
-         * */
         customers.stream()
-                .map(customer -> CompletableFuture.supplyAsync(() -> customerRepository.save(customer), executorService))
+                .map(this::saveCustomer)
                 .toList()
                 .forEach(CompletableFuture::join);
 
         return customerRepository.findAll();
+    }
+
+    private CompletableFuture<Customer> saveCustomer(Customer customer) {
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                //On réserve un permit
+                semaphore.acquire();
+                log.info("thread {}", Thread.currentThread());
+                return customerRepository.save(customer);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } finally {
+                //On libère le permit pour un autre VirtualThread
+                semaphore.release();
+            }
+
+        }, executorService);
     }
 }
